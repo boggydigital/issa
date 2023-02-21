24ff09c3139341a12b81ea1361e6c0e80d8ab1b7
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2023-02-20 21:20:37 -0800

    Adding helper to determine optimal sampling

---
+++ find_optimal_sampling.go
@@ -0,0 +1,56 @@
+package issa
+
+import (
+	"image"
+	"math"
+	"os"
+)
+
+const maxSample = 64
+
+func FindOptimalSampling(imageSources []string) (int, int, error) {
+
+	images := make([]image.Image, len(imageSources))
+	for i, imgSrc := range imageSources {
+
+		fi, err := os.Open(imgSrc)
+		if err != nil {
+			return -1, 0, err
+		}
+
+		di, _, err := image.Decode(fi)
+		if err != nil {
+			return -1, 0, err
+		}
+
+		images[i] = di
+	}
+
+	minLen := math.MaxInt
+	minSample := -1
+	totalLen := 0
+
+	for s := 2; s < maxSample; s++ {
+
+		totalLen = 0
+		for _, img := range images {
+
+			ig := GIFImage(img, StdPalette(), s)
+
+			dhi, err := Dehydrate(ig)
+			if err != nil {
+				return minSample, 0, err
+			}
+
+			totalLen += len(dhi)
+		}
+
+		if totalLen < minLen {
+			minLen = totalLen
+			minSample = s
+		}
+
+	}
+
+	return minSample, totalLen, nil
+}
